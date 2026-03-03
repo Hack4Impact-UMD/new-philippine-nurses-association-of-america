53c9b90b44e6eaa7b43b343361b35e377c5fd60c
Amanuel Abiy <amanuelabiy41@gmail.com>
2026-03-02 23:03:14 -0500

    Added template for env

--- pnaa/app/(app)/dashboard/page.tsx
+++ pnaa/app/(app)/dashboard/page.tsx
@@ -35,6 +35,7 @@ export default function DashboardPage() {
     "events",
     eventConstraints
   );
+  
 
   const campaignConstraints = useMemo(
     () => [where("archived", "==", false), orderBy("date", "desc"), limit(5)],
